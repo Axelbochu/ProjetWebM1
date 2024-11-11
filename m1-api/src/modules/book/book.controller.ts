7b69264f579d57946a23bc0f2bc60ff8f768beae
Axel Bochu <axel.bochu@student.junia.com>
2024-11-11 19:07:13 +0100

    debut book

--- m1-api/src/modules/book/book.controller.ts
+++ m1-api/src/modules/book/book.controller.ts
@@ -7,27 +7,48 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
-import { BookService } from './book.service';
-import { CreateBookDto, UpdateBookDto, NewAdviceDto } from './book.dto';
 import { BookId } from '../database/entities/book.entity';
+import { CreateBookDto, NewAdviceDto, UpdateBookDto } from './book.dto';
+import { BookPresenter } from './book.presenter';
+import { BookService } from './book.service';
+import { DetailsBookPresenter } from './detailsBook.presenter';
 
 @Controller('books')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
   @Get()
-  public async getBooks() {
-    return this.bookService.listBooks();
+  public async getBooks(): Promise<BookPresenter[]> {
+    const books = await this.bookService.listBooks();
+
+    return await Promise.all(
+      books.map(async (book) => {
+        const author = book.author;
+        const averageRating = 0; //valeur temporaire
+
+        return BookPresenter.from(book, author, averageRating);
+      }),
+    );
   }
 
+  //TODO : nouveau presenter
   @Get(':id')
-  public async getBookById(@Param('id') id: BookId) {
-    return this.bookService.getBook(id);
+  public async getBookById(
+    @Param('id') id: BookId,
+  ): Promise<DetailsBookPresenter> {
+    const book = await this.bookService.getBook(id);
+    console.log(book);
+
+    return DetailsBookPresenter.from(book, book.author);
   }
 
   @Post()
-  public async createBook(@Body() input: CreateBookDto): Promise<string> {
-    return this.bookService.createBook(input);
+  public async createBook(
+    @Body() input: CreateBookDto,
+  ): Promise<DetailsBookPresenter> {
+    const book = await this.bookService.createBook(input);
+
+    return DetailsBookPresenter.from(book, book.author);
   }
 
   @Get(':search')
--- m1-api/src/modules/book/book.reposiory.ts
+++ m1-api/src/modules/book/book.reposiory.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
+import { AuthorEntity } from '../database/entities/author.entity';
 import { BookEntity, BookId } from '../database/entities/book.entity';
-import { BookModel } from './book.model';
+import { BookModel, CreateBookModel } from './book.model';
 
 @Injectable()
 export class BookRepository {
   private readonly bookRepository = this.dataSource.getRepository(BookEntity);
+  private readonly authorRepository =
+    this.dataSource.getRepository(AuthorEntity);
 
   constructor(private readonly dataSource: DataSource) {}
 
@@ -24,5 +27,17 @@ export class BookRepository {
     });
 
     return book;
+  }
+
+  public async createBook(input: CreateBookModel): Promise<BookModel> {
+    const author = await this.authorRepository.findOneOrFail({
+      where: { id: input.authorId },
+    });
+
+    const book = this.bookRepository.create({ ...input, author });
+
+    const savedBook = await this.bookRepository.save(book);
+
+    return savedBook;
   }
 }
