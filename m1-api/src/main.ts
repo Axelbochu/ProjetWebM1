c1d67d560e6499d30b68b45f28e29ea1c4273f9f
Axel Bochu <90176811+Axelbochu@users.noreply.github.com>
2024-11-16 20:16:10 +0100

    images api

--- m1-api/src/main.ts
+++ m1-api/src/main.ts
@@ -1,14 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { config } from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { config } from 'dotenv';
+import { join } from 'path';
+import { AppModule } from './app.module';
 
 config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Validation des DTOs
   app.useGlobalPipes(new ValidationPipe());
+
+  // CORS pour permettre les requêtes cross-origin
   app.enableCors();
+
+  // Servir les fichiers statiques (par ex., images uploadées)
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads', // Les images seront accessibles via /uploads/<nom du fichier>
+  });
+
+  // Écoute sur le port configuré dans .env
   await app.listen(process.env.PORT);
 
   console.log(`🚀 API listening on PORT ${process.env.PORT}`);
